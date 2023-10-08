dbc38167eeb4c30c461412b82bc9f0517d16e078
vmnavarro94 <vm.navarro94@gmail.com>
2023-10-08 17:28:30 -0600

    fix(category): permissions for creation/edition

--- src/routes/category.router.ts
+++ src/routes/category.router.ts
@@ -2,6 +2,8 @@ import { Elysia } from 'elysia'
 import { Category as CategoryType } from '../types/category.type'
 import CategoryService from '../services/category.service'
 import { isAuthenticated } from '../middlewares/auth.handler'
+import { hasPermission } from '../hooks'
+import { Permission } from '../types/permission.type'
 
 const service = new CategoryService()
 
@@ -9,11 +11,6 @@ export const categoryRouter = (app: Elysia) =>
   app.group('/categories', (app) =>
     app
       .use(isAuthenticated)
-      .post('/', async ({ body, set }) => {
-        const newCategory = await service.create(body as CategoryType)
-        set.status = 201
-        return newCategory
-      })
       .get('/', async ({ set }) => {
         const categories = await service.find()
         set.status = 200
@@ -24,17 +21,29 @@ export const categoryRouter = (app: Elysia) =>
         set.status = 200
         return category
       })
-      .patch('/:id', async ({ params: { id }, body: category, set }) => {
-        const updatedCategory = await service.update(
-          id,
-          category as CategoryType
-        )
-        set.status = 200
-        return updatedCategory
-      })
-      .delete('/:id', async ({ params: { id }, set }) => {
-        const deletedCategory = await service.delete(id)
-        set.status = 200
-        return deletedCategory
-      })
+      .guard(
+        {
+          beforeHandle: hasPermission([Permission.MANAGE_PRODUCTS])
+        },
+        (app) =>
+          app
+            .post('/', async ({ body, set }) => {
+              const newCategory = await service.create(body as CategoryType)
+              set.status = 201
+              return newCategory
+            })
+            .patch('/:id', async ({ params: { id }, body: category, set }) => {
+              const updatedCategory = await service.update(
+                id,
+                category as CategoryType
+              )
+              set.status = 200
+              return updatedCategory
+            })
+            .delete('/:id', async ({ params: { id }, set }) => {
+              const deletedCategory = await service.delete(id)
+              set.status = 200
+              return deletedCategory
+            })
+      )
   )
